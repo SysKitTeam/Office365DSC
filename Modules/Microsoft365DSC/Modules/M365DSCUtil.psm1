950a133c1498e3cc5ee9372ecc7c18a6ebd9e846
Vinko Bedek <VinkoBedek@hotmail.com>
2020-08-25 16:26:46 +0200

    small auth fix

--- Modules/Microsoft365DSC/Modules/M365DSCUtil.psm1
+++ Modules/Microsoft365DSC/Modules/M365DSCUtil.psm1
@@ -1169,7 +1169,7 @@ function New-M365DSCConnection
 
     # TODO: m365-merge check if we can use the newer mscloudloginassistant
     # for now we are staying on our version and just bypassing all of the new logic
-    Test-MSCloudLogin -Platform $Platform
+    Test-MSCloudLogin -Platform $Platform -CloudCredential $InboundParameters.GlobalAdminAccount
     return "ServicePrincipal"
 
     # Case both authentication methods are attempted
