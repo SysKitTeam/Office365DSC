8079f2b0c82b80adbd1082f3e8b49aacf96eba26
Vinko Bedek <VinkoBedek@hotmail.com>
2020-09-10 10:46:48 +0200

    live updates test

--- CopyToTrace.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item 'C:\GitProjects\SysKitTrace\SysKit.SPDocKit.Office365\UtilProjects\MSCloudLoginAssistant\**' -Force -Recurse
-Copy-Item 'C:\GitProjects\MSCloudLoginAssistant-SysKit\Modules\MSCloudLoginAssistant\**' 'C:\GitProjects\SysKitTrace\SysKit.SPDocKit.Office365\UtilProjects\MSCloudLoginAssistant\' -Force -Recurse
-
-
-Remove-Item 'C:\GitProjects\SysKitTrace\SysKit.SPDocKit.Office365\UtilProjects\Microsoft365DSC\**' -Force -Recurse
-Copy-Item 'C:\GitProjects\Office365DSC-SysKit\Modules\Microsoft365DSC\**' 'C:\GitProjects\SysKitTrace\SysKit.SPDocKit.Office365\UtilProjects\Microsoft365DSC\' -Force -Recurse
